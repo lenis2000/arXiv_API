8fabd5768686761c2fe2fa35e00d0dd45d7d8f36
Leonid Petrov <lenia.petrov@gmail.com>
2017-04-11 13:51:14 -0400

    even better presentation

--- arXiv_API_recents.py
+++ arXiv_API_recents.py
@@ -52,7 +52,7 @@ for entry in feed.entries:
     target_file.write('---\n')
     target_file.write('\n')
 
-    target_file.write(', '.join(author.name for author in entry.authors).encode('utf-8') + ', "<i>')
+    target_file.write('<b>' + '</b>, <b>'.join(author.name for author in entry.authors).encode('utf-8') + '</b>, "<i>')
     target_file.write(entry.title.encode('utf-8').replace('\n', '')  + '</i>" ([arXiv](')
     target_file.write(entry.id + '))\n')
 
